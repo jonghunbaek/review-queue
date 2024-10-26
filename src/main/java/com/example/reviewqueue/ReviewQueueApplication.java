2c1de39b29c25e0e5e20d4c28e7a3d58976c2b18
jonghunbaek <qor6028@naver.com>
2024-10-26 23:34:40 +0900

    Initial commit

---
+++ src/main/java/com/example/reviewqueue/ReviewQueueApplication.java
@@ -0,0 +1,13 @@
+package com.example.reviewqueue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewQueueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReviewQueueApplication.class, args);
+    }
+
+}
