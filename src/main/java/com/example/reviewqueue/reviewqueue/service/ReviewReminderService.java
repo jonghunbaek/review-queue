271c75c0a010752299b782db84ff2fea74b1d353
jonghunbaek <qor6028@naver.com>
2024-11-12 01:25:00 +0900

    feat : 매일 오전 5시에 알림을 보내는 메서드 추가(선언부)

--- src/main/java/com/example/reviewqueue/reviewqueue/service/ReviewReminderService.java
+++ src/main/java/com/example/reviewqueue/reviewqueue/service/ReviewReminderService.java
@@ -2,8 +2,11 @@ package com.example.reviewqueue.reviewqueue.service;
 
 import com.example.reviewqueue.reviewqueue.repository.SseEmitterRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
@@ -11,6 +14,7 @@ public class ReviewReminderService {
 
     public static final long TIME_OUT_MILLIS = 30 * 60 * 1000L;
 
+    private final ReviewQueueService reviewQueueService;
     private final SseEmitterRepository sseEmitterRepository;
 
     public SseEmitter subscribe(Long userId) {
@@ -20,5 +24,13 @@ public class ReviewReminderService {
         emitter.onTimeout(() -> sseEmitterRepository.deleteByUserId(userId));
 
         return emitter;
+    }
+
+    @Scheduled(cron = "0 0 5 * * ?")
+    public void sendReviewsReminder() {
+        List<SseEmitter> emitters = sseEmitterRepository.findAll();
+        // TODO ::
+        //  1. 알림을 보낼 복습 데이터 가져오기
+        //  2. 알림보낸 내역 저장(SSE는 실시간 알림을 위함이므로 로그인하지 않은 사용자에 대해서도 알림을 보내야함)
     }
 }
