66cb94bae807d73ca892e5f2b1e96dc36ae5afba
jonghunbaek <qor6028@naver.com>
2024-11-12 01:20:53 +0900

    feat : sse 구독 컨트롤러 추가

---
+++ src/main/java/com/example/reviewqueue/reviewqueue/controller/ReviewReminderController.java
@@ -0,0 +1,21 @@
+package com.example.reviewqueue.reviewqueue.controller;
+
+import com.example.reviewqueue.reviewqueue.service.ReviewReminderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+@RequestMapping("/reviews/reminder")
+@RequiredArgsConstructor
+@RestController
+public class ReviewReminderController {
+
+    private final ReviewReminderService reviewReminderService;
+
+    @GetMapping(value = "/subscribe", produces = "text/event-stream")
+    public SseEmitter subscribe(Long memberId) {
+        return reviewReminderService.subscribe(memberId);
+    }
+}
