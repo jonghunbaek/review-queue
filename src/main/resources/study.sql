276292a4c7115c605dc3da963cb28e94f99018f6
jonghunbaek <qor6028@naver.com>
2024-11-14 01:58:44 +0900

    docs : sql 도메인별로 분리

--- src/main/resources/study.sql
+++ src/main/resources/study.sql
@@ -1,5 +1,2 @@
-insert into member(email, nickname)
-values ('iphone@gmail.com', '테스트 유저1');
-
 insert into study(study_type, title, description, member_id)
 values ('BOOK', 'Real MySQL', 'MySQL 관련 도서', 1);
