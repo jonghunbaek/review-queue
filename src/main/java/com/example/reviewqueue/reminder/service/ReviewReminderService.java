267d931d8484619012fab4ea8ccd246a791df806
jonghunbaek <qor6028@naver.com>
2024-11-16 17:45:02 +0900

    refactor : 도메인 관계에 따른 서비스 클래스 분리 및 이름 변경

---
+++ src/main/java/com/example/reviewqueue/reminder/service/ReviewReminderService.java
@@ -0,0 +1,42 @@
+package com.example.reviewqueue.reminder.service;
+
+import com.example.reviewqueue.member.domain.Member;
+import com.example.reviewqueue.member.repository.MemberRepository;
+import com.example.reviewqueue.reminder.domain.ReviewReminder;
+import com.example.reviewqueue.reminder.repository.ReviewReminderRepository;
+import com.example.reviewqueue.review.service.ReviewService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class ReviewReminderService {
+
+    private final ReviewService reviewService;
+    private final SseService sseService;
+
+    private final MemberRepository memberRepository;
+    private final ReviewReminderRepository reminderRepository;
+
+    // TODO :: 나중에 JdbcTemplate을 활용해 벌크 INSERT로 변경하기
+    @Scheduled(cron = "0 0 5 * * ?")
+    public void addReminder() {
+        LocalDate today = LocalDate.now();
+        List<Long> memberIds = reviewService.findMemberIdsByReviewDate(today);
+        List<Member> members = memberRepository.findAllById(memberIds);
+
+        List<ReviewReminder> reminders = members.stream()
+                .map(member -> new ReviewReminder(today, member))
+                .toList();
+
+        reminderRepository.saveAll(reminders);
+
+        sseService.sendReminder(memberIds);
+    }
+}
--- src/main/java/com/example/reviewqueue/reminder/service/SseService.java
+++ src/main/java/com/example/reviewqueue/reminder/service/SseService.java
@@ -1,15 +1,12 @@
 package com.example.reviewqueue.reminder.service;
 
 import com.example.reviewqueue.reminder.repository.SseEmitterRepository;
-import com.example.reviewqueue.review.service.ReviewService;
-import com.example.reviewqueue.review.service.dto.ReviewData;
 import lombok.RequiredArgsConstructor;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
-import java.time.LocalDate;
 import java.util.List;
+import java.util.Map;
 
 @RequiredArgsConstructor
 @Service
@@ -17,7 +14,6 @@ public class SseService {
 
     public static final long TIME_OUT_MILLIS = 30 * 60 * 1000L;
 
-    private final ReviewService reviewService;
     private final SseEmitterRepository sseEmitterRepository;
 
     public SseEmitter subscribe(Long userId) {
@@ -29,13 +25,12 @@ public class SseService {
         return emitter;
     }
 
-    // TODO :: 복습할 내용이 있는 경우에만 알림을 보내고, 실제 데이터는 알림을 클릭했을 때 조회하도록 변경하기
-    @Scheduled(cron = "0 0 5 * * ?")
-    public void sendReviewsReminder() {
-        List<SseEmitter> emitters = sseEmitterRepository.findAll();
-        List<ReviewData> reviewData = reviewService.findAllReviewDataByDate(LocalDate.now());
+    public void sendReminder(List<Long> memberIds) {
+        Map<Long, SseEmitter> emittersByMemberId = sseEmitterRepository.findAllByMemberIds(memberIds);
 
-        // TODO ::
-        //  2. 알림보낸 내역 저장(SSE는 실시간 알림을 위함이므로 로그인하지 않은 사용자에 대해서도 알림을 보내야함)
+    }
+
+    private String createEventId(Long userId) {
+        return userId.toString() + System.currentTimeMillis();
     }
 }
