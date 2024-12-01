bd8bcfee5add13bc9ff1796083ce4e02e7daaf6c
jonghunbaek <qor6028@naver.com>
2024-12-01 18:07:08 +0900

    docs : jwt, 카카오 소셜 로그인 관련 설정 추가

--- src/main/java/com/example/reviewqueue/common/config/jpa/JpaConfig.java
+++ src/main/java/com/example/reviewqueue/common/config/jpa/JpaConfig.java
@@ -1,4 +1,4 @@
-package com.example.reviewqueue.common.config;
+package com.example.reviewqueue.common.config.jpa;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
