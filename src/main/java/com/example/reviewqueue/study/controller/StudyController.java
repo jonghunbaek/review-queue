4b9e553a4679b993df3bc03ed83c3625a5111e3a
jonghunbaek <qor6028@naver.com>
2024-12-06 09:14:03 +0900

    feat : 학습 단일, 전체 조회 API 추가

--- src/main/java/com/example/reviewqueue/study/controller/StudyController.java
+++ src/main/java/com/example/reviewqueue/study/controller/StudyController.java
@@ -3,11 +3,11 @@ package com.example.reviewqueue.study.controller;
 import com.example.reviewqueue.common.resolver.AuthenticatedMember;
 import com.example.reviewqueue.study.controller.dto.StudySaveInfo;
 import com.example.reviewqueue.study.service.StudyService;
+import com.example.reviewqueue.study.service.dto.StudyInfo;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/studies")
@@ -16,8 +16,28 @@ public class StudyController {
 
     private final StudyService studyService;
 
+    /**
+     *  학습 등록
+     */
     @PostMapping
     public void saveStudy(@RequestBody StudySaveInfo studySaveInfo, @AuthenticatedMember Long memberId) {
         studyService.save(studySaveInfo.toStudySave(), memberId);
     }
+
+    /**
+     *  학습 단일 조회
+     */
+    @GetMapping("/{studyId}")
+    public StudyInfo getStudyById(@PathVariable("studyId") Long studyId) {
+        return studyService.findStudyInfoBy(studyId);
+    }
+
+    /**
+     *  학습 전체 조회
+     */
+    @GetMapping
+    public List<StudyInfo> getAllStudiesById(@AuthenticatedMember Long memberId) {
+        return studyService.findAllStudyInfosBy(memberId);
+    }
+    
 }
