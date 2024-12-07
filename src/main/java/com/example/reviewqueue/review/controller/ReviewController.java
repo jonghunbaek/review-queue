769fcdc05dacf83b255bca0d6b7888576ba19985
jonghunbaek <qor6028@naver.com>
2024-12-07 13:08:31 +0900

    feat : 복습 조건을 저장하는 API 추가

--- src/main/java/com/example/reviewqueue/review/controller/ReviewController.java
+++ src/main/java/com/example/reviewqueue/review/controller/ReviewController.java
@@ -1,4 +1,23 @@
 package com.example.reviewqueue.review.controller;
 
+import com.example.reviewqueue.common.resolver.AuthenticatedMember;
+import com.example.reviewqueue.review.service.ReviewService;
+import com.example.reviewqueue.review.service.dto.ReviewQueueSave;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api/v1/reviews")
+@RequiredArgsConstructor
+@RestController
 public class ReviewController {
+
+    private final ReviewService reviewService;
+
+    @PostMapping
+    public void postReviews(@RequestBody ReviewQueueSave reviewQueueSave, @AuthenticatedMember Long memberId) {
+        reviewService.save(reviewQueueSave, memberId);
+    }
 }
